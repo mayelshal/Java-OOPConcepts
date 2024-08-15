1b8bc4eb99409f1b67ad14ccd7cbac4eea8e097c
mayelshal <mayelshal1810@gmail.com>
2024-08-15 19:17:30 +0300

    first commit

---
+++ src/main/java/KeyWords/FinalKeyword.java
@@ -0,0 +1,16 @@
+package KeyWords;
+
+class Test
+{
+    final int x =10;
+
+}
+
+public class FinalKeyword {
+    public static void main(String args[])
+    {
+        Test t = new Test();
+        //you can not change the value of x
+
+    }
+}
